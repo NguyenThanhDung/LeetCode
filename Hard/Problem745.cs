41ebc7286dc0472c6cdda9510f0c9cfbbbe72f8d
Thanh Dung Nguyen <dung.nguyenthanh@hotmail.com>
2023-02-16 18:10:28 -0600

    Problem 745 - Initial solution

--- Hard/Problem745.cs
+++ Hard/Problem745.cs
@@ -6,18 +6,34 @@ public class Problem745
         WordFilter wordFilter = new WordFilter(words);
         int index = wordFilter.F("a", "e");
         Console.WriteLine(index == 0);
+
+        words = new string[] { "WordFilter" };
+        wordFilter = new WordFilter(words);
+        index = wordFilter.F("f", "");
+        Console.WriteLine(index == -1);
     }
 
     public class WordFilter
     {
+        private string[] words;
+
         public WordFilter(string[] words)
         {
-
+            this.words = words;
         }
 
         public int F(string pref, string suff)
         {
-            return 0;
+            int index = -1;
+            for (int i = words.Length - 1; i >= 0; i--)
+            {
+                if (words[i].StartsWith(pref) && words[i].EndsWith(suff))
+                {
+                    index = i;
+                    break;
+                }
+            }
+            return index;
         }
     }
 }
