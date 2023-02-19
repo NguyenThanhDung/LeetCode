31eec316fcb19052f0d8cddb0f9b4a2bada03c35
Thanh Dung Nguyen <dung.nguyenthanh@hotmail.com>
2023-02-18 18:26:09 -0600

    Problem 1300 - Add template

---
+++ Medium/Problem1300.cs
@@ -0,0 +1,14 @@
+public class Problem1300
+{
+    public Problem1300()
+    {
+        Console.WriteLine(FindBestValue(new int[] { 4, 9, 3 }, 10) == 3);
+        Console.WriteLine(FindBestValue(new int[] { 2, 3, 5 }, 10) == 3);
+        Console.WriteLine(FindBestValue(new int[] { 60864, 25176, 27249, 21296, 20204 }, 56803) == 11361);
+    }
+
+    public int FindBestValue(int[] arr, int target)
+    {
+        return 0;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 new Problem989();
 new Problem745();
+new Problem1300();
