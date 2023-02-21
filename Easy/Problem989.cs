7c026026a543a68aa6844e46dbec5e9c9395a5b4
Thanh Dung Nguyen <dung.nguyenthanh@hotmail.com>
2023-02-21 09:17:29 -0600

    Problem 989 - Use Enumerable method to compare arrays

--- Easy/Problem989.cs
+++ Easy/Problem989.cs
@@ -11,26 +11,14 @@ public class Problem989
         Console.WriteLine(maxLength(new int[] { 1, 2, 1, 5 }, 806) == 4);
         Console.WriteLine(maxLength(new int[] { 2, 1, 5 }, 8064) == 4);
 
-        Console.WriteLine(compareArray(numberToArray(34, 2), new int[] { 3, 4 }));
-        Console.WriteLine(compareArray(numberToArray(181, 3), new int[] { 1, 8, 1 }));
-        Console.WriteLine(compareArray(numberToArray(806, 3), new int[] { 8, 0, 6 }));
-        Console.WriteLine(compareArray(numberToArray(1806, 4), new int[] { 1, 8, 0, 6 }));
+        Console.WriteLine(Enumerable.SequenceEqual(numberToArray(34, 2), new int[] { 3, 4 }));
+        Console.WriteLine(Enumerable.SequenceEqual(numberToArray(181, 3), new int[] { 1, 8, 1 }));
+        Console.WriteLine(Enumerable.SequenceEqual(numberToArray(806, 3), new int[] { 8, 0, 6 }));
+        Console.WriteLine(Enumerable.SequenceEqual(numberToArray(1806, 4), new int[] { 1, 8, 0, 6 }));
 
         // Console.WriteLine(Enumerable.SequenceEqual(AddToArrayForm(new int[] { 1, 2, 0, 0 }, 34), new int[] { 1, 2, 3, 4 }));
         // Console.WriteLine(Enumerable.SequenceEqual(AddToArrayForm(new int[] { 2, 7, 4 }, 181), new int[] { 4, 5, 5 }));
         // Console.WriteLine(Enumerable.SequenceEqual(AddToArrayForm(new int[] { 2, 1, 5 }, 806), new int[] { 1, 0, 2, 1 }));
-    }
-
-    private bool compareArray(int[] a1, int[] a2)
-    {
-        if (a1.Length != a2.Length)
-            return false;
-        for (int i = 0; i < a1.Length; i++)
-        {
-            if (a1[i] != a2[i])
-                return false;
-        }
-        return true;
     }
 
     private int maxLength(int[] num, int k)
