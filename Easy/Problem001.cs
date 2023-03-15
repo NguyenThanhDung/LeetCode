671c8dbc2f167cba32ec6a85371c698381c42af0
Thanh Dung Nguyen <dung.nguyenthanh@hotmail.com>
2023-03-15 08:49:03 -0500

    Problem 001 - Resolve problem

--- Easy/Problem001.cs
+++ Easy/Problem001.cs
@@ -13,6 +13,19 @@ public class Problem001
 
     public int[] TwoSum(int[] nums, int target)
     {
-        return new int[2] { 0, 1 };
+        int[] result = new int[] { -1, -1 };
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return result;
     }
 }
