b1779c04bd10a87f3d24136a6a4e4e99ba4f46ad
Thanh Dung Nguyen <dung.nguyenthanh@hotmail.com>
2023-03-24 09:55:57 -0500

    Problem 1300 - Rename mothod

--- Medium/Problem1300.cs
+++ Medium/Problem1300.cs
@@ -4,13 +4,13 @@ public class Problem1300
 {
     public Problem1300()
     {
-        Console.WriteLine(FindBestValue2(new int[] { 4, 9, 3 }, 10) == 3);
-        Console.WriteLine(FindBestValue2(new int[] { 2, 3, 5 }, 10) == 5);
-        Console.WriteLine(FindBestValue2(new int[] { 60864, 25176, 27249, 21296, 20204 }, 56803) == 11361);
+        Console.WriteLine(FindBestValue(new int[] { 4, 9, 3 }, 10) == 3);
+        Console.WriteLine(FindBestValue(new int[] { 2, 3, 5 }, 10) == 5);
+        Console.WriteLine(FindBestValue(new int[] { 60864, 25176, 27249, 21296, 20204 }, 56803) == 11361);
 
         Input input = new Input("Medium", "Input1300.txt");
         DateTime start = DateTime.Now;
-        Console.WriteLine(FindBestValue2(input.arr, input.target) == 4);
+        Console.WriteLine(FindBestValue(input.arr, input.target) == 4);
         Console.WriteLine(DateTime.Now - start);
     }
 
@@ -48,40 +48,6 @@ public class Problem1300
             }
         }
     }
-
-
-    // private int Max(int[] arr)
-    // {
-    //     int max = int.MinValue;
-    //     foreach (int elem in arr)
-    //     {
-    //         if (elem > max)
-    //             max = elem;
-    //     }
-    //     return max;
-    // }
-
-    // private int Sum(int[] arr)
-    // {
-    //     int sum = 0;
-    //     foreach (int elem in arr)
-    //         sum += elem;
-    //     return sum;
-    // }
-
-    // private void ReduceArray(int[] arr, int value)
-    // {
-    //     for (int i = 0; i < arr.Length; i++)
-    //     {
-    //         if (arr[i] > value)
-    //             arr[i] = value;
-    //     }
-    // }
-
-    // public int GetRestrictedValue(int[] arr, int index, int restrictValue)
-    // {
-    //     return (arr[index] < restrictValue) ? arr[index] : restrictValue;
-    // }
 
     public int GetRestrictedSum(int[] arr, int restrictValue)
     {
@@ -161,23 +127,7 @@ public class Problem1300
         }
     }
 
-    // public int FindBestValue(int[] arr, int target)
-    // {
-    //     Differ differ = new Differ();
-    //     int value = Max(arr);
-    //     int sum = Sum(arr);
-    //     differ.SetValueAndDiff(value, Math.Abs(sum - target));
-    //     while (sum > target)
-    //     {
-    //         value--;
-    //         ReduceArray(arr, value);
-    //         sum = Sum(arr);
-    //         differ.SetValueAndDiff(value, Math.Abs(sum - target));
-    //     }
-    //     return differ.GetBestValue();
-    // }
-
-    public int FindBestValue2(int[] arr, int target)
+    public int FindBestValue(int[] arr, int target)
     {
         int value = int.MaxValue;
         Differ differ = new Differ();
