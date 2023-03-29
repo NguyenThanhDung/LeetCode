926b29e3ce9771bd83a24f3af1ee10a4394c0f8f
Thanh Dung Nguyen <dung.nguyenthanh@hotmail.com>
2023-03-29 12:15:26 -0500

    Problem 745 - Changed property name of json

--- Hard/Problem745.cs
+++ Hard/Problem745.cs
@@ -72,14 +72,14 @@ public class Problem745
                     Name = root.GetProperty("Name").GetString(),
                     Age = root.GetProperty("Age").GetInt32(),
                     Email = root.GetProperty("Email").GetString(),
-                    Word = JsonSerializer.Deserialize<string>(root.GetProperty("Code")[0].GetRawText())
+                    Word = JsonSerializer.Deserialize<string>(root.GetProperty("Input")[0].GetRawText())
                 };
 
                 person.Code = new List<List<string>>();
-                int length = root.GetProperty("Code").GetArrayLength();
+                int length = root.GetProperty("Input").GetArrayLength();
                 for(int i = 1; i < length; i++)
                 {
-                    List<string> pair = JsonSerializer.Deserialize<List<string>>(root.GetProperty("Code")[i].GetRawText());
+                    List<string> pair = JsonSerializer.Deserialize<List<string>>(root.GetProperty("Input")[i].GetRawText());
                     person.Code.Add(pair);
                 }
 
