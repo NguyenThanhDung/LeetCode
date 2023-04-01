cff3c7379645216663e7452e46a8c3cfafdc1b0e
Thanh Dung Nguyen <dung.nguyenthanh@hotmail.com>
2023-04-01 16:25:53 -0500

    Problem 745 - Parse real data

--- Hard/Problem745.cs
+++ Hard/Problem745.cs
@@ -11,8 +11,7 @@ public class Problem745
         int index = wordFilter.F("a", "e");
         Console.WriteLine(index == 0);
 
-        Input input = new Input("Hard", "input5.json");
-        // Input input = new Input("Hard", "input.txt");
+        Input input = new Input("Hard", "input.json");
         // foreach (string word in input.words)
         //     Console.Write(word + " ");
         // Console.WriteLine();
@@ -47,17 +46,8 @@ public class Problem745
 
     public class Input
     {
-        public string[] words;
-        public string[][] filters;
-
-        public class Person
-        {
-            public string Name { get; set; }
-            public int Age { get; set; }
-            public string Email { get; set; }
             public List<string> Words { get; set; }
             public List<List<string>> Filters { get; set; }
-        }
 
         public Input(string level, string filename)
         {
@@ -67,37 +57,16 @@ public class Problem745
             using (JsonDocument doc = JsonDocument.Parse(text))
             {
                 JsonElement root = doc.RootElement;
-                Person person = new Person
-                {
-                    Name = root.GetProperty("Name").GetString(),
-                    Age = root.GetProperty("Age").GetInt32(),
-                    Email = root.GetProperty("Email").GetString(),
-                    Words = JsonSerializer.Deserialize<List<string>>(root.GetProperty("Words").GetRawText())
-                };
+                Words = JsonSerializer.Deserialize<List<string>>(root.GetProperty("Words").GetRawText());
 
-                person.Filters = new List<List<string>>();
+                Filters = new List<List<string>>();
                 int length = root.GetProperty("Filters").GetArrayLength();
                 for(int i = 0; i < length; i++)
                 {
                     List<string> pair = JsonSerializer.Deserialize<List<string>>(root.GetProperty("Filters")[i].GetRawText());
-                    person.Filters.Add(pair);
+                    Filters.Add(pair);
                 }
-
-                Console.WriteLine();
             }
-
-            // string[] lines = File.ReadAllLines(path);
-            // JsonElement element = JsonSerializer.Deserialize<JsonElement>(lines[1]);
-            // words = element[0][0]
-            //     .EnumerateArray()
-            //     .Select(x => new string(x.GetString()))
-            //     .ToArray();
-            // filters = element[1]
-            //     .EnumerateArray()
-            //     .Select(x => x.EnumerateArray()
-            //         .Select(y => y.GetString())
-            //         .ToArray())
-            //     .ToArray();
         }
     }
 }
