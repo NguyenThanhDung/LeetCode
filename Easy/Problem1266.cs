7020acbe5976bb2893e0913be7a625b28224e420
Thanh Dung Nguyen <dung.nguyenthanh@hotmail.com>
2023-04-05 13:58:29 -0500

    Problem 1266 - Initialize

---
+++ Easy/Problem1266.cs
@@ -0,0 +1,12 @@
+public class Problem1266
+{
+    public Problem1266()
+    {
+        Console.WriteLine(MinTimeToVisitAllPoints(new int[][] { new int[] { 1, 1 }, new int[] { 3, 4 }, new int[] { -1, 0 } }) == 7);
+    }
+
+    public int MinTimeToVisitAllPoints(int[][] points)
+    {
+        return 0;
+    }
+}
