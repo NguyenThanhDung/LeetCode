c35439f9e70753c7fb046bfdd16a6080751fa168
Thanh Dung Nguyen <dung.nguyenthanh@hotmail.com>
2023-04-11 21:54:07 -0500

    Problem 1345 - Initialize

---
+++ Hard/Problem1345.cs
@@ -0,0 +1,14 @@
+public class Problem1345
+{
+    public Problem1345()
+    {
+        Console.WriteLine(MinJumps(new int[] { 100, -23, -23, 404, 100, 23, 23, 23, 3, 404 }) == 3);
+        Console.WriteLine(MinJumps(new int[] { 7 }) == 0);
+        Console.WriteLine(MinJumps(new int[] { 7, 6, 9, 6, 9, 6, 9, 7 }) == 1);
+    }
+
+    public int MinJumps(int[] arr)
+    {
+        return 0;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-new Problem001();
-new Problem009();
-new Problem014();
-new Problem104();
-new Problem211();
-new Problem745();
-new Problem953();
-new Problem989();
-new Problem1300();
-new Problem1379();
-new Problem2423();
+// new Problem001();
+// new Problem009();
+// new Problem014();
+// new Problem104();
+// new Problem211();
+// new Problem745();
+// new Problem953();
+// new Problem989();
+// new Problem1300();
+new Problem1345();
+// new Problem1379();
+// new Problem2423();
