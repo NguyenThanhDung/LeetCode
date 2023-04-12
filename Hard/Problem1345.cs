7382b60a89e2b99350a47ab1d331e337972e7424
Thanh Dung Nguyen <dung.nguyenthanh@hotmail.com>
2023-04-11 22:34:44 -0500

    Problem 1345 - Add method to get the list of jumpable positions

--- Hard/Problem1345.cs
+++ Hard/Problem1345.cs
@@ -9,6 +9,22 @@ public class Problem1345
 
     public int MinJumps(int[] arr)
     {
+        List<int>[] jumpablePosstions = GetJumpablePositions(arr);
         return 0;
+    }
+
+    public List<int>[] GetJumpablePositions(int[] arr)
+    {
+        List<int>[] jumpablePosstions = new List<int>[arr.Length];
+        for (int i = 0; i < arr.Length; i++)
+        {
+            jumpablePosstions[i] = new List<int>();
+            for (int j = 0; j < arr.Length; j++)
+            {
+                if (i != j && arr[i] == arr[j])
+                    jumpablePosstions[i].Add(j);
+            }
+        }
+        return jumpablePosstions;
     }
 }
