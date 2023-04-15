83057e8dd5f2424ed07f6294a5a04e07b18b7480
Thanh Dung Nguyen <dung.nguyenthanh@hotmail.com>
2023-04-15 18:13:18 -0500

    Add back previous problems

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+new Problem001();
+new Problem009();
+new Problem014();
+new Problem104();
+new Problem211();
+new Problem745();
+new Problem953();
+new Problem989();
 new Problem1266();
+new Problem1300();
+new Problem1379();
+new Problem2423();
