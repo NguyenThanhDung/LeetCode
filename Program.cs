c0287a88afa323c5662a8b3bc13cc2c1e66cd583
Thanh Dung Nguyen <dung.nguyenthanh@hotmail.com>
2023-04-15 17:33:21 -0500

    Problem 1266 - Resolved

--- Program.cs
+++ Program.cs
@@ -1,13 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-new Problem001();
-new Problem009();
-new Problem014();
-new Problem104();
-new Problem211();
-new Problem745();
-new Problem953();
-new Problem989();
-new Problem1300();
-new Problem1379();
-new Problem2423();
+new Problem1266();
