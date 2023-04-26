31514e45163f486425e975e2823307e21fb2eb77
Thanh Dung Nguyen <dung.nguyenthanh@hotmail.com>
2023-04-26 10:05:16 -0500

    Add compile conditions for each problem

--- Program.cs
+++ Program.cs
@@ -1,15 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿#define ALL
 
+#if ALL || PROBLEM_001
 new Problem001();
+#endif
+
+#if ALL || PROBLEM_009
 new Problem009();
+#endif
+
+#if ALL || PROBLEM_014
 new Problem014();
+#endif
+
+#if ALL || PROBLEM_104
 new Problem104();
+#endif
+
+#if ALL || PROBLEM_211
 new Problem211();
+#endif
+
+#if ALL || PROBLEM_745
 new Problem745();
+#endif
+
+#if ALL || PROBLEM_953
 new Problem953();
+#endif
+
+#if ALL || PROBLEM_989
 new Problem989();
+#endif
+
+#if ALL || PROBLEM_1217
 new Problem1217();
+#endif
+
+#if ALL || PROBLEM_1266
 new Problem1266();
+#endif
+
+#if ALL || PROBLEM_1300
 new Problem1300();
+#endif
+
+#if ALL || PROBLEM_1379
 new Problem1379();
+#endif
+
+#if ALL || PROBLEM_2423
 new Problem2423();
+#endif
