c6da646811967b8284962716d7a71b5d3aa39a12
Thanh Dung Nguyen <dung.nguyenthanh@hotmail.com>
2023-04-29 16:34:29 -0500

    Problem 1345 - First try

--- Hard/Problem1345/Problem1345.cs
+++ Hard/Problem1345/Problem1345.cs
@@ -9,8 +9,40 @@ public class Problem1345
 
     public int MinJumps(int[] arr)
     {
-        List<int>[] jumpablePosstions = GetJumpablePositions(arr);
-        return 0;
+        List<int>[] jumpableSteps = GetJumpablePositions(arr);
+        Stack<int> steps = new Stack<int>();
+        return JumpRecursively(arr, 0, steps, jumpableSteps, arr.Length);
+    }
+
+    private int JumpRecursively(int[] arr, int index, Stack<int> steps, List<int>[] jumpableSteps, int minStepCount)
+    {
+        steps.Push(index);
+
+        if (steps.Count > minStepCount || index == arr.Length)
+        {
+            steps.Pop();
+            return minStepCount;
+        }
+
+        int nextStep = index + 1;
+        if (nextStep < arr.Length && steps.Contains(nextStep) == false)
+            minStepCount = JumpRecursively(arr, nextStep, steps, jumpableSteps, minStepCount);
+
+        nextStep = index - 1;
+        if (nextStep >= 0 && steps.Contains(nextStep) == false)
+            minStepCount = JumpRecursively(arr, nextStep, steps, jumpableSteps, minStepCount);
+
+        List<int> jumpableStepsOfIndex = jumpableSteps[index];
+        foreach (int step in jumpableStepsOfIndex)
+        {
+            if (steps.Contains(step) == false)
+            {
+                minStepCount = JumpRecursively(arr, step, steps, jumpableSteps, minStepCount);
+            }
+        }
+
+        steps.Pop();
+        return minStepCount;
     }
 
     public List<int>[] GetJumpablePositions(int[] arr)
