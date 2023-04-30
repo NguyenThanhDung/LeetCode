cd477a0d7848d3365121573509658a77d47f56fb
Thanh Dung Nguyen <dung.nguyenthanh@hotmail.com>
2023-04-30 17:12:47 -0500

    Problem 345 - Update the solution

--- Easy/Problem345.cs
+++ Easy/Problem345.cs
@@ -2,29 +2,28 @@ public class Problem345
 {
     public Problem345()
     {
-        Console.WriteLine(ReverseVowels("hello"));  // holle
-        Console.WriteLine(ReverseVowels("leetcode"));    // leotcede
+        Console.WriteLine(ReverseVowels("hello"));      // holle
+        Console.WriteLine(ReverseVowels("leetcode"));   // leotcede
+        Console.WriteLine(ReverseVowels("a."));         // a.
     }
 
     public string ReverseVowels(string s)
     {
-        List<char> reverse = new List<char>();
-        int j = s.Length - 1;
+        Stack<char> reverseVowels = new Stack<char>();
+        for (int i = 0; i < s.Length; i++)
+            if (IsVowels(s[i]))
+                reverseVowels.Push(s[i]);
+
+        List<char> result = new List<char>();
         for (int i = 0; i < s.Length; i++)
         {
             if (IsVowels(s[i]))
-            {
-                while (j > 1 && IsVowels(s[j]) == false)
-                    j--;
-                reverse.Add(s[j]);
-                j--;
-            }
+                result.Add(reverseVowels.Pop());
             else
-            {
-                reverse.Add(s[i]);
-            }
+                result.Add(s[i]);
         }
-        return new string(reverse.ToArray());
+
+        return new string(result.ToArray());
     }
 
     private bool IsVowels(char c)
