8427591f446a4c22f05781d6d9aa2eca0b6b3d91
Thanh Dung Nguyen <thanhdung.nguyen@miu.edu>
2023-05-23 16:35:08 -0500

    Problem 375 - Add test case

--- Medium/Problem375.cs
+++ Medium/Problem375.cs
@@ -2,6 +2,13 @@ public class Problem375
 {
     public Problem375()
     {
-        Console.WriteLine("Problem375");
+        Console.WriteLine(GetMoneyAmount(10) == 16);
+        Console.WriteLine(GetMoneyAmount(1) == 0);
+        Console.WriteLine(GetMoneyAmount(2) == 1);
+    }
+
+    public int GetMoneyAmount(int n)
+    {
+        return -1;
     }
 }
