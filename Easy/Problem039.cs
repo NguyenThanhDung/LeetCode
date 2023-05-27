702e57a34daba032d3407e4cded9e4031aba722e
Thanh Dung Nguyen <thanhdung.nguyen@miu.edu>
2023-05-27 17:09:35 -0500

    Problem 039 - First attempt

--- Easy/Problem039.cs
+++ Easy/Problem039.cs
@@ -2,12 +2,46 @@ public class Problem039
 {
     public Problem039()
     {
-        Console.WriteLine(CombinationSum(new int[]{2, 3, 6, 7}, 7));
-        Console.WriteLine(CombinationSum(new int[]{2, 3, 5}, 8));
-        Console.WriteLine(CombinationSum(new int[]{2}, 1));
+        IList<IList<int>> result = CombinationSum(new int[]{2, 3, 6, 7}, 7);
+        result = CombinationSum(new int[]{2, 3, 5}, 8);
+        result = CombinationSum(new int[]{2}, 1);
+        Console.WriteLine();
     }
 
     public IList<IList<int>> CombinationSum(int[] candidates, int target) {
-        return new List<IList<int>>();
+        List<IList<int>> result = new List<IList<int>>();
+        List<int> indexes = new List<int>();
+        List<int> combination = new List<int>();
+
+        int sum = -1;
+        do
+        {
+            int index = -1;
+            while (sum < target)
+            {
+                index = indexes.Count == 0 ? 0 : indexes.Last();
+                indexes.Add(index);
+                combination.Add(candidates[index]);
+                sum = combination.Sum();
+            }
+            if (sum == target)
+            {
+                result.Add(combination.ToList());
+            }
+
+            indexes.RemoveAt(indexes.Count - 1);
+            combination.RemoveAt(combination.Count - 1);
+
+            if(indexes.Count > 0)
+            {
+                index = indexes.Last() + 1;
+                indexes[indexes.Count - 1] = index;
+                combination[combination.Count - 1] = candidates[index];
+                sum = combination.Sum();
+            }
+        }
+        while(indexes.Count > 0);
+
+        return result;
     }
 }
