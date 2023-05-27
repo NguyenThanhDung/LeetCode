37000222bc99553540224d0d1f8a286640b5a51c
Thanh Dung Nguyen <thanhdung.nguyen@miu.edu>
2023-05-27 15:58:47 -0500

    Problem 039 - Show result

--- Easy/Problem039.cs
+++ Easy/Problem039.cs
@@ -2,6 +2,12 @@ public class Problem039
 {
     public Problem039()
     {
-        Console.WriteLine("Problem039");
+        Console.WriteLine(CombinationSum(new int[]{2, 3, 6, 7}, 7));
+        Console.WriteLine(CombinationSum(new int[]{2, 3, 5}, 8));
+        Console.WriteLine(CombinationSum(new int[]{2}, 1));
+    }
+
+    public IList<IList<int>> CombinationSum(int[] candidates, int target) {
+        return new List<IList<int>>();
     }
 }
