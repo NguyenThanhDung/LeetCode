679e13735399cc23a997626a7f92b75f8fc05140
Dung Nguyen Thanh <dung.nguyenthanh@hotmail.com>
2023-07-05 21:24:37 -0500

    Problem 136 - Add test cases

--- Easy/Problem136.cs
+++ Easy/Problem136.cs
@@ -2,6 +2,13 @@ public class Problem136
 {
     public Problem136()
     {
-        Console.WriteLine("Problem136");
+        Console.WriteLine(SingleNumber(new int[] { 2, 2, 1 }) == 1);
+        Console.WriteLine(SingleNumber(new int[] { 4, 1, 2, 1, 2 }) == 4);
+        Console.WriteLine(SingleNumber(new int[] { 1 }) == 1);
+    }
+
+    public int SingleNumber(int[] nums)
+    {
+        return 0;
     }
 }
