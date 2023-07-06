13e272e59a484edd3f09c7a91ba8e175c53316f9
Dung Nguyen Thanh <dung.nguyenthanh@hotmail.com>
2023-07-05 21:31:55 -0500

    Problem 136 - Resolve

--- Easy/Problem136.cs
+++ Easy/Problem136.cs
@@ -9,6 +9,27 @@ public class Problem136
 
     public int SingleNumber(int[] nums)
     {
-        return 0;
+        Dictionary<int, int> appearance = new Dictionary<int, int>();
+        foreach (int e in nums)
+        {
+            if (appearance.ContainsKey(e))
+                appearance[e] += 1;
+            else
+                appearance.Add(e, 1);
+        }
+
+        foreach (KeyValuePair<int, int> kvp in appearance)
+        {
+            if (kvp.Value == 1)
+                return kvp.Key;
+        }
+
+        return -1;
     }
 }
+
+// {2, 2, 1}
+// i    nums[i] appearance
+// 0    2       {2: 1}
+// 1    2       {2: 2}
+// 2    1       {2: 2, 1: 1}
