8c1ce617d086ba9a722bca7470219936a0ce5e9f
Dung Nguyen Thanh <dung.nguyenthanh@hotmail.com>
2023-07-15 16:29:52 -0500

    Problem 2663 - First try

--- Hard/Problem2663.cs
+++ Hard/Problem2663.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 public class Problem2663
 {
     public Problem2663()
@@ -6,7 +8,43 @@ public class Problem2663
         Console.WriteLine(SmallestBeautifulString("dc", 4).Equals(""));
     }
 
-    public string SmallestBeautifulString(string s, int k) {
-        return "";
+    public string SmallestBeautifulString(string s, int k)
+    {
+        do
+        {
+            s = IncreaseOneUnit(s, k);
+        } while (IsBeautiful(s) == false || s.Equals("") == false);
+        return s;
+    }
+
+    private string IncreaseOneUnit(string s, int k)
+    {
+        StringBuilder sbResult = new StringBuilder();
+        bool shouldIncrease = true;
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            char nextChar = (char)(((int)s[i]) + (shouldIncrease ? 1 : 0));
+            if (nextChar - 'a' > k)
+            {
+                nextChar = 'a';
+                shouldIncrease = true;
+            }
+            else
+            {
+                shouldIncrease = false;
+            }
+            sbResult.Insert(0, nextChar);
+        }
+        return sbResult.ToString();
+    }
+
+    private bool IsBeautiful(string s)
+    {
+        for (int i = 0; i < s.Length - 2; i++)
+        {
+            if (s[i] == s[i + 1])
+                return false;
+        }
+        return true;
     }
 }
