f58aa730fd2a5566e43734eae267a07f7e89a807
Dung Nguyen Thanh <dung.nguyenthanh@hotmail.com>
2023-07-17 19:30:24 -0500

    Problem 2663 - Fix bug

--- Hard/Problem2663.cs
+++ Hard/Problem2663.cs
@@ -45,7 +45,7 @@ public class Problem2663
 
     private bool IsBeautiful(string s)
     {
-        for (int i = 0; i < s.Length - 2; i++)
+        for (int i = 0; i < s.Length - 1; i++)
         {
             if (s[i] == s[i + 1])
                 return false;
