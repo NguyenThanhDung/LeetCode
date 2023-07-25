d9a5d9731c850ece5a2ded4d06bb60d47d4da857
Dung Nguyen Thanh <dung.nguyenthanh@hotmail.com>
2023-07-25 16:07:41 -0500

    Problem 1282 - Add more test case

--- Medium/Problem1282.cs
+++ Medium/Problem1282.cs
@@ -6,6 +6,9 @@ public class Problem1282
     {
         IList<IList<int>> result = GroupThePeople(new int[] { 3, 3, 3, 3, 3, 1, 3 });
         Console.WriteLine(FormatResult(result));
+
+        result = GroupThePeople(new int[] { 2, 1, 3, 3, 3, 2 });
+        Console.WriteLine(FormatResult(result));
     }
 
     public IList<IList<int>> GroupThePeople(int[] groupSizes)
