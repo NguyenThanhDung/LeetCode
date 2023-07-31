ed49c9fa1eadc298aada47f3707580d66820da91
Dung Nguyen Thanh <dung.nguyenthanh@hotmail.com>
2023-07-31 14:40:34 -0500

    Problem 066 - Add testing class

--- Easy/Problem066.cs
+++ Easy/Problem066.cs
@@ -2,9 +2,9 @@ public class Problem066
 {
     public Problem066()
     {
-        Console.WriteLine(PlusOne(new int[] { 1, 2, 3 }));
-        Console.WriteLine(PlusOne(new int[] { 4, 3, 2, 1 }));
-        Console.WriteLine(PlusOne(new int[] { 9 }));
+        Console.WriteLine(Testing.CompareArrays(PlusOne(new int[] { 1, 2, 3 }), new int[] { 1, 2, 4 }));
+        Console.WriteLine(Testing.CompareArrays(PlusOne(new int[] { 4, 3, 2, 1 }), new int[] { 4, 3, 2, 2 }));
+        Console.WriteLine(Testing.CompareArrays(PlusOne(new int[] { 9 }), new int[] { 1, 0 }));
     }
 
     public int[] PlusOne(int[] digits)
