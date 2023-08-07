aebf9914e13e5192d60e7910df2d351538a29aab
Dung Nguyen Thanh <dung.nguyenthanh@hotmail.com>
2023-08-06 21:38:29 -0500

    Problem 226 - Resolve

--- Easy/Problem226.cs
+++ Easy/Problem226.cs
@@ -3,6 +3,19 @@ public class Problem226
     public Problem226()
     {
         Tree tree = new Tree(new int?[] { 4, 2, 7, 1, 3, 6, 9 });
+        TreeNode invertRoot = InvertTree(tree.Root);
         Console.WriteLine();
+    }
+
+    public TreeNode InvertTree(TreeNode root)
+    {
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        if (root.left != null)
+            InvertTree(root.left);
+        if (root.right != null)
+            InvertTree(root.right);
+        return root;
     }
 }
