c9657dc6e800d8d735ef5441fe60b03ed97f32c0
Dung Nguyen Thanh <dung.nguyenthanh@hotmail.com>
2023-08-06 21:39:31 -0500

    Problem 226 - Add more test case

--- Easy/Problem226.cs
+++ Easy/Problem226.cs
@@ -5,6 +5,10 @@ public class Problem226
         Tree tree = new Tree(new int?[] { 4, 2, 7, 1, 3, 6, 9 });
         TreeNode invertRoot = InvertTree(tree.Root);
         Console.WriteLine();
+
+        tree = new Tree(new int?[] { 2, 1, 3 });
+        invertRoot = InvertTree(tree.Root);
+        Console.WriteLine();
     }
 
     public TreeNode InvertTree(TreeNode root)
