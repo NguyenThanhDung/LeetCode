ad07805156eb1c01fcea7788944a329fde0a8523
Dung Nguyen Thanh <dung.nguyenthanh@hotmail.com>
2023-08-11 22:24:34 -0500

    Problem 128 - Try to fix more test case

--- Medium/Problem128.cs
+++ Medium/Problem128.cs
@@ -4,28 +4,29 @@ public class Problem128
     {
         Console.WriteLine(LongestConsecutive(new int[] { 100, 4, 200, 1, 3, 2 }) == 4);
         Console.WriteLine(LongestConsecutive(new int[] { 0, 3, 7, 2, 5, 8, 4, 6, 0, 1 }) == 9);
+        Console.WriteLine(LongestConsecutive(new int[] { 9, 1, 4, 7, 3, -1, 0, 5, 8, -1, 6 }) == 7);
     }
 
     public int LongestConsecutive(int[] nums)
     {
-        if(nums.Length == 0)
-            return 0;
-        Dictionary<int, int> processedNums = new Dictionary<int, int>();
-        Dictionary<int, int> ranges = new Dictionary<int, int>();
+        Dictionary<int, int> distinctNums = new Dictionary<int, int>();
         foreach (var n in nums)
         {
-            if (!processedNums.ContainsKey(n))
+            if (!distinctNums.ContainsKey(n))
+                distinctNums.Add(n, 0);
+        }
+
+        Dictionary<int, int> ranges = new Dictionary<int, int>();
+        foreach (var n in distinctNums.Keys)
+        {
+            int minOfPossibleSequence = n - distinctNums.Keys.Count + 1;
+            int maxOfPossibleSequence = n + distinctNums.Keys.Count - 1;
+            for (int i = minOfPossibleSequence; i <= maxOfPossibleSequence; i++)
             {
-                processedNums.Add(n, 0);
-                int minOfPossibleSequence = n - nums.Length + 1;
-                int maxOfPossibleSequence = n + nums.Length - 1;
-                for (int i = minOfPossibleSequence; i <= maxOfPossibleSequence; i++)
-                {
-                    if (ranges.ContainsKey(i))
-                        ranges[i]++;
-                    else
-                        ranges.Add(i, 1);
-                }
+                if (ranges.ContainsKey(i))
+                    ranges[i]++;
+                else
+                    ranges.Add(i, 1);
             }
         }
         int maxValue = int.MinValue;
