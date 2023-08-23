df3c726ad209e77dbd5405fddc33f979e771227a
Dung Nguyen Thanh <dung.nguyenthanh@hotmail.com>
2023-08-22 21:36:19 -0500

    Problem 2369 - Find all possible sub array lengths

--- Medium/Problem2369.cs
+++ Medium/Problem2369.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 public class Problem2369
 {
     public Problem2369()
@@ -8,6 +10,46 @@ public class Problem2369
 
     public bool ValidPartition(int[] nums)
     {
+        var possibleSubArrayLengths = FindPossibleSubArrayLengths(nums.Length);
+        foreach (var possibleSubArrayLength in possibleSubArrayLengths)
+        {
+            var sb = new StringBuilder();
+            sb.Append("[");
+            foreach (var length in possibleSubArrayLength)
+            {
+                sb.Append(length + " ");
+            }
+            sb.Append("]");
+            Console.WriteLine(sb.ToString());
+        }
         return false;
+    }
+
+    private IList<IList<int>> FindPossibleSubArrayLengths(int arrayLength)
+    {
+        var lengths = new List<IList<int>>();
+        var currentLengths = new List<int>();
+        FindPossibleSubArrayLengthsRecursively(arrayLength, lengths, currentLengths, 2);
+        FindPossibleSubArrayLengthsRecursively(arrayLength, lengths, currentLengths, 3);
+        return lengths;
+    }
+
+    private void FindPossibleSubArrayLengthsRecursively(int remaining, IList<IList<int>> lengths, IList<int> currentLengths, int size)
+    {
+        remaining -= size;
+        if (remaining < 0)
+            return;
+
+        currentLengths.Add(size);
+        if (remaining == 0)
+        {
+            lengths.Add(currentLengths);
+        }
+        else
+        {
+            FindPossibleSubArrayLengthsRecursively(remaining, lengths, currentLengths, 2);
+            FindPossibleSubArrayLengthsRecursively(remaining, lengths, currentLengths, 3);
+        }
+        currentLengths.RemoveAt(currentLengths.Count - 1);
     }
 }
