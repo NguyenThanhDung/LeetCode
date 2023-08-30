283b1ecde23aca60d51a09af4ca95bb9fe701003
Dung Nguyen Thanh <dung.nguyenthanh@hotmail.com>
2023-08-30 15:57:54 -0500

    Problem 1345 - Add more test data

--- Hard/Problem1345/Problem1345.cs
+++ Hard/Problem1345/Problem1345.cs
@@ -5,6 +5,17 @@ public class Problem1345
         Console.WriteLine(MinJumps(new int[] { 100, -23, -23, 404, 100, 23, 23, 23, 3, 404 }) == 3);
         Console.WriteLine(MinJumps(new int[] { 7 }) == 0);
         Console.WriteLine(MinJumps(new int[] { 7, 6, 9, 6, 9, 6, 9, 7 }) == 1);
+
+        var filePath = Path.Join(Directory.GetCurrentDirectory(), "Hard", "Problem1345", "data2.txt");
+        var lines = File.ReadAllLines(filePath);
+        var intArray = new int[lines.Length];
+        int intValue = 0;
+        for (int i = 0; i < intArray.Length; i++)
+        {
+            if (Int32.TryParse(lines[i], out intValue))
+                intArray[i] = intValue;
+        }
+        Console.WriteLine("");
     }
 
     public int MinJumps(int[] arr)
