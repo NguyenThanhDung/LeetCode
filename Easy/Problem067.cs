e28f18e97b53b887b7f34fe514795acd75c7002f
Dung Nguyen Thanh <dung.nguyenthanh@hotmail.com>
2023-09-06 15:16:14 -0500

    Problem 67 - Add test cases

--- Easy/Problem067.cs
+++ Easy/Problem067.cs
@@ -2,6 +2,12 @@ public class Problem067
 {
     public Problem067()
     {
-        Console.WriteLine("Problem067");
+        Console.WriteLine(AddBinary("11", "1").Equals("100"));
+        Console.WriteLine(AddBinary("1010", "1011").Equals("10101"));
+    }
+
+    public string AddBinary(string a, string b)
+    {
+        return "";
     }
 }
