e3daa842a75b1192a31d79c222d4b6ba7529bc4e
Dung Nguyen Thanh <dung.nguyenthanh@hotmail.com>
2023-09-16 15:28:58 -0500

    Problem 1768 - Optimize

--- Easy/Problem1768.cs
+++ Easy/Problem1768.cs
@@ -11,16 +11,13 @@ public class Problem1768
     public string MergeAlternately(string word1, string word2)
     {
         StringBuilder sb = new StringBuilder();
-        int minLength = word1.Length < word2.Length ? word1.Length : word2.Length;
-        for(int i = 0; i < minLength; i++)
+        for (int i = 0; i < word1.Length || i < word2.Length; i++)
         {
-            sb.Append(word1[i]);
-            sb.Append(word2[i]);
+            if (i < word1.Length)
+                sb.Append(word1[i]);
+            if (i < word2.Length)
+                sb.Append(word2[i]);
         }
-        if(word1.Length < word2.Length)
-            sb.Append(word2.Substring(minLength));
-        else
-            sb.Append(word1.Substring(minLength));
         return sb.ToString();
     }
 }
