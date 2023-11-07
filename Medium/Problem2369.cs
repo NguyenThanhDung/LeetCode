176e4ffbaf47ea6096294fdc2489f93cfdddf278
Dung Nguyen Thanh <dung.nguyenthanh@hotmail.com>
2023-11-06 20:23:52 -0600

    Problem 2369 - Try new algorithm

--- Medium/Problem2369.cs
+++ Medium/Problem2369.cs
@@ -10,56 +10,38 @@ public class Problem2369
 
     public bool ValidPartition(int[] nums)
     {
-        var possibleSubArrayLengths = FindPossibleSubArrayLengths(nums.Length);
-        foreach (var possibleSubArrayLength in possibleSubArrayLengths)
+        if (nums.Length == 0)
+            return true;
+        if (nums.Length == 1)
+            return false;
+        int[] sub = new int[2];
+        Array.Copy(nums, sub, sub.Length);
+        if (IsSubArrayValid(sub))
         {
-            int currentIndex = 0;
-            bool isValid = true;
-            foreach (var length in possibleSubArrayLength)
-            {
-                var subArray = new int[length];
-                Array.Copy(nums, currentIndex, subArray, 0, length);
-                isValid = IsValid(subArray);
-                if (!isValid)
-                    break;
-                currentIndex += length;
-            }
-            if (isValid)
-                return true;
-        }
-        return false;
-    }
-
-    private IList<IList<int>> FindPossibleSubArrayLengths(int arrayLength)
-    {
-        var lengths = new List<IList<int>>();
-        var currentLengths = new List<int>();
-        FindPossibleSubArrayLengthsRecursively(arrayLength, lengths, currentLengths, 2);
-        FindPossibleSubArrayLengthsRecursively(arrayLength, lengths, currentLengths, 3);
-        return lengths;
-    }
-
-    private void FindPossibleSubArrayLengthsRecursively(int remaining, IList<IList<int>> lengths, IList<int> currentLengths, int size)
-    {
-        remaining -= size;
-        if (remaining < 0)
-            return;
-
-        currentLengths.Add(size);
-        if (remaining == 0)
-        {
-            var validLengths = new List<int>(currentLengths);
-            lengths.Add(validLengths);
+            int[] remaining = new int[nums.Length - 2];
+            Array.Copy(nums, 2, remaining, 0, remaining.Length);
+            return ValidPartition(remaining);
         }
         else
         {
-            FindPossibleSubArrayLengthsRecursively(remaining, lengths, currentLengths, 2);
-            FindPossibleSubArrayLengthsRecursively(remaining, lengths, currentLengths, 3);
+            if (nums.Length == 2)
+                return false;
+            sub = new int[3];
+            Array.Copy(nums, sub, sub.Length);
+            if (IsSubArrayValid(sub))
+            {
+                int[] remaining = new int[nums.Length - 3];
+                Array.Copy(nums, 3, remaining, 0, remaining.Length);
+                return ValidPartition(remaining);
+            }
+            else
+            {
+                return false;
+            }
         }
-        currentLengths.RemoveAt(currentLengths.Count - 1);
     }
 
-    private bool IsValid(int[] array)
+    public bool IsSubArrayValid(int[] array)
     {
         if (array.Length == 2)
         {
