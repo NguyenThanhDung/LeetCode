160df90b739ffde55fa7d46be3e9c0243e6d75e8
Dung Nguyen Thanh <dung.nguyenthanh@hotmail.com>
2024-01-27 15:31:28 -0600

    Problem 530 - Add test cases

--- Easy/Problem530.cs
+++ Easy/Problem530.cs
@@ -3,6 +3,15 @@ public class Problem530
 {
     public Problem530()
     {
-        Console.WriteLine("Problem530");
+        Tree tree = new Tree(new int?[] { 4, 2, 6, 1, 3 });
+        Console.WriteLine(getMinimumDifference(tree.Root) == 1);
+
+        tree = new Tree(new int?[] { 1, 0, 48, null, null, 12, 49 });
+        Console.WriteLine(getMinimumDifference(tree.Root) == 1);
+    }
+
+    public int getMinimumDifference(TreeNode root)
+    {
+        return 0;
     }
 }
