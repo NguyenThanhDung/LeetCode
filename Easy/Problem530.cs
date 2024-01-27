863166b9a2626b11b299c41185b6b00175c2c62b
Dung Nguyen Thanh <dung.nguyenthanh@hotmail.com>
2024-01-27 15:03:19 -0600

    Problem 530 - Initialize

---
+++ Easy/Problem530.cs
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/minimum-absolute-difference-in-bst/description/
+public class Problem530
+{
+    public Problem530()
+    {
+        Console.WriteLine("Problem530");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 {
     public static void Main()
     {
-        int problemNumber = 2396;
+        int problemNumber = 530;
         String className = problemNumber < 1000 ? String.Format("Problem{0,3:D3}", problemNumber) : "Problem" + problemNumber;
         try
         {
