a0a4adda84f320fca863f38387968d41379723df
Dung Nguyen Thanh <dung.nguyenthanh@hotmail.com>
2024-01-31 11:39:12 -0600

    Problem 530 - Refactor

--- Easy/Problem530.cs
+++ Easy/Problem530.cs
@@ -18,29 +18,29 @@ public class Problem530
 
     public int getMinimumDifference(TreeNode root)
     {
-        (int, int, int) info = getMinimumDifferenceRecursively(root);
+        (int, int, int) info = GetMinimumDifferenceRecursively(root);
         return info.Item1;
     }
 
-    private (int minDifference, int minValue, int maxValue) getMinimumDifferenceRecursively(TreeNode node)
+    private (int minDifference, int minValue, int maxValue) GetMinimumDifferenceRecursively(TreeNode node)
     {
         if (node == null)
             return (MAX_VALUE, MAX_VALUE, MIN_VALUE);
-        (int, int, int) leftInfo = getMinimumDifferenceRecursively(node.left);
-        (int, int, int) rightInfo = getMinimumDifferenceRecursively(node.left);
-        int minDifference = min(leftInfo.Item1, rightInfo.Item1, node.val - leftInfo.Item3, rightInfo.Item2 - node.val);
+        (int, int, int) leftInfo = GetMinimumDifferenceRecursively(node.left);
+        (int, int, int) rightInfo = GetMinimumDifferenceRecursively(node.left);
+        int minDifference = Min(leftInfo.Item1, rightInfo.Item1, node.val - leftInfo.Item3, rightInfo.Item2 - node.val);
         return (minDifference, leftInfo.Item2, rightInfo.Item3);
     }
 
-    private int min(int num1, int num2, int num3, int num4)
+    private int Min(int leftMinDifference, int rightMinDifference, int leftMaxValue, int rightMinValue)
     {
-        int min = num1;
-        if (num2 < min)
-            min = num2;
-        if (num3 < min)
-            min = num3;
-        if (num4 < min)
-            min = num4;
+        int min = leftMinDifference;
+        if (rightMinDifference < min)
+            min = rightMinDifference;
+        if (leftMaxValue < min)
+            min = leftMaxValue;
+        if (rightMinValue < min)
+            min = rightMinValue;
         return min;
     }
 }
