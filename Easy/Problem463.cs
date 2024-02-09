a26a56a848330b0902f312a1935c3ee4f5e19b84
Dung Nguyen Thanh <dung.nguyenthanh@hotmail.com>
2024-02-08 22:18:00 -0600

    Problem 463 - Add test cases

--- Easy/Problem463.cs
+++ Easy/Problem463.cs
@@ -2,6 +2,24 @@ public class Problem463
 {
     public Problem463()
     {
-        Console.WriteLine("Problem463");
+        int[][] grid = new int[4][]
+            {
+                new int[] { 0, 1, 0, 0 },
+                new int[] { 1, 1, 1, 0 },
+                new int[] { 0, 1, 0, 0 },
+                new int[] { 1, 1, 0, 0 }
+            };
+        Console.WriteLine(IslandPerimeter(grid) == 16);
+
+        grid = new int[1][] { new int[] { 1 } };
+        Console.WriteLine(IslandPerimeter(grid) == 4);
+
+        grid = new int[1][] { new int[] { 1, 0 } };
+        Console.WriteLine(IslandPerimeter(grid) == 4);
+    }
+
+    public int IslandPerimeter(int[][] grid)
+    {
+        return 0;
     }
 }
